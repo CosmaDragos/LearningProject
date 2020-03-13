d70a12b2528d6efb2893c576a84ef3cfc79b1ff3
Dragos Cosma <dragos.cosma@fortech.ro>
2020-03-13 15:18:20 +0200

    Project setup

---
+++ LearningProject/LearningProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningProject
+//
+//  Created by Dragos Cosma on 13/03/2020.
+//  Copyright © 2020 Dragos Cosma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
